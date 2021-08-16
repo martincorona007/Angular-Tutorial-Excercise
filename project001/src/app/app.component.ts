b3515631fe0536052da0c1183eb7f6fe9c7a6c9e
Martin <martincorona007@hotmail.com>
2021-08-16 17:43:59 -0500

    adding p1

---
+++ project001/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre='';
+  apellido="";
+  /*counter = 1;
+  increase(){
+    this.counter ++;
+  }
+  decrement(){
+    this.counter--;
+  }*/
+  //name = 'Rodriguez Pablo Valboa';
+  //age = 10;
+  //email = 'rpabloescobar@gmail.com';
+  //salaries = [1700,1600,1900];
+  //activo =  true;
+  
+  /*esActivo(){
+    if(this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+  ultimos3Sueldos(){
+    let suma = 0;
+    for( let x = 0; x < this.sueldos.length; x++)
+      suma+=this.sueldos[x];
+    return suma;
+  }*/
+}
