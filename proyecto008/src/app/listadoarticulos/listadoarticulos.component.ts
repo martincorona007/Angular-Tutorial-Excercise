654cf54bd2b813088e6911f0f0737dae6c08fcf0
Martin <martincorona007@hotmail.com>
2021-08-18 13:38:26 -0500

    13. adding files

---
+++ proyecto008/src/app/listadoarticulos/listadoarticulos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit , Input} from '@angular/core';
+
+@Component({
+  selector: 'app-listadoarticulos',
+  templateUrl: './listadoarticulos.component.html',
+  styleUrls: ['./listadoarticulos.component.css']
+})
+export class ListadoarticulosComponent implements OnInit {
+  @Input() datos: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
