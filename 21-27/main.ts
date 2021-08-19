218a65b3d3f21f180053c4244abe6f24515db574
Martin <martincorona007@hotmail.com>
2021-08-19 16:59:58 -0500

    24 adding files

--- 21-27/main.ts
+++ 21-27/main.ts
@@ -101,4 +101,85 @@ class Article{
 let article1: Article;
 article1 = new Article(1,'Potatoes',12.5);
 article1.print();
+//***************24********************
 
+//anonymous functions
+const funcSumar = function(value1: number, value2: number): number{
+    return value1 + value2;
+}
+console.log(funcSumar(4,9));
+//optional parameters
+function add(value1: number,value2: number,value3: number): number{
+    if(value3)
+        return value1 + value2 + value3;
+    else 
+        return value1 + value2
+}
+console.log(add(5,4));
+console.log(add(5,4,3));
+//Rest Parameters
+//The Rest parameter concept is achieved by preceding the parameter name with three dots:
+  //puede tener otros parámetros pero se deben declarar antes
+function add (... values: number []) { 
+    let sum = 0; 
+    for (let x = 0; x <values.length; x ++) 
+      sum + = values ​​[x]; 
+    return sum; 
+  } 
+  console.log (add (10, 2, 44, 3)); 
+  console.log (sum (1, 2)); 
+  console.log (sum ());
+  //Operador Spread
+  //contrario a rest
+
+  function add(value1: number,value2: number,value3: number){
+      return valor1 + valor2 + valor 3;
+  }
+  const vec: number[] = [10,20,30];
+  const s = sumar(...vec);
+  console.log(s);
+//parametros tipo union
+// alamcenar mas de un tipo de dato
+let edad: number | string;
+edad=34;
+console.log(edad);
+edad='20 años';
+console.log(edad);
+
+function sumar(valor1: number | string, valor2: number | string ): number | string  {
+    if (typeof valor1 ==='number' && typeof valor2 ==='number')
+      return valor1+valor2;
+    else 
+      return valor1.toString() + valor2.toString();
+  }
+  
+  console.log(sumar(4, 5));
+  console.log(sumar('Hola ', 2));
+  console.log(sumar('Hola ', 'Mundo'));
+//funciones callback
+function operar(valor1: number, valor2: number, func: (x: number, y:number)=>number): number {
+    return func(valor1, valor2);
+  }
+  
+  console.log(operar(3, 7, (x: number,y: number): number => {
+    return x+y;
+  }))
+  
+  console.log(operar(3, 7, (x: number,y: number): number => {
+    return x-y;
+  }))
+  
+  console.log(operar(3, 7, (x: number,y: number): number => {
+    return x*y;
+  }))
+  type Operacion=(x: number, y:number)=>number;
+//
+  function operar(valor1: number, valor2: number, func: Operacion): number {
+    return func(valor1, valor2);
+  }
+  
+  console.log(operar(3, 7, (x: number,y: number): number => {  return x+y;  }))
+  
+  console.log(operar(3, 7, (x: number,y: number): number => {  return x-y;  }))
+  
+  console.log(operar(3, 7, (x: number,y: number): number => {  return x*y;  }))
