8ad251f371a93d61271a045864e58f07298e6e65
Martin <martincorona007@hotmail.com>
2021-08-18 19:50:55 -0500

    16 adding files

---
+++ proyecto011/src/app/juegodados/juegodados.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juegodados',
+  templateUrl: './juegodados.component.html',
+  styleUrls: ['./juegodados.component.css']
+})
+export class JuegodadosComponent implements OnInit {
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado: string = '';
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  ngOnInit(){
+    
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)    
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }
+}
