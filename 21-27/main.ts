1bed7d6a016fec05ca02d01075149934d6557320
Martin <martincorona007@hotmail.com>
2021-08-19 15:29:16 -0500

    23 adding files

--- 21-27/main.ts
+++ 21-27/main.ts
@@ -1,9 +1,15 @@
 //***************21 - 22***************
+int vivo = 4;
+let _age: number = 23;
+let _day: string;
+let _dta = "mary";
+let _sdt; 
+
 let vector: number [] = [1, 4, 2];
 vector.push (33);
 for (let element of vector)
   console.log (item);
-//***************23********************
+//***************22********************
 //normal array
 let vector: number [] = [1, 4, 2];
 vector.push (33);
@@ -49,4 +55,50 @@ export class AppComponent {
         alert(message);
     }
 }
+
+//***************23********************
+class Person{
+    _name: string;
+    _age: number;
+    constructor(name: string,age: number){
+        this._name = name;
+        this._age = age; 
+    }
+    print(){
+        console.log(`Name: ${this.name} and age: ${this.age}`);
+    }
 }
+let person1: Person;
+person1 = new Person('John',45);
+person1.print();
+
+//definition and initialization of properties in the constructor parameters
+class Persona{
+    constructor(public name: string,public age: number){}
+    print(){
+        console.log(`Name: ${this.name} and age: ${this.age}`);
+    }
+}
+let person1: Person;
+person1 = new Person('John',45);
+person1.print();
+//readonly modifier
+//the property value can only be loaded in the constructor or when defining it and then it cannot be modified either from a method of the class or outside the class.
+class Article{
+    readonly code: number;
+    description: string;
+    price: number;
+    constructor(code: number,description: string,price: number){
+        this.code = code;
+        this.description = description;
+        this.price = price;
+    }
+    print(){
+        this.code = 7 //error
+        console.log(`Code ${this.code} Description: ${this.description} Price ${this.price}`);
+    }
+}
+let article1: Article;
+article1 = new Article(1,'Potatoes',12.5);
+article1.print();
+
